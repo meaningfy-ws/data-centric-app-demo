2fb01e37f81f5c57f0f0675659aebbe841db2789
Dumitru <d.prijilevschi@gmail.com>
2024-04-17 18:41:06 +0300

    implement create and delete gherkin features

---
+++ Makefile
@@ -0,0 +1,3 @@
+
+run-behave:
+	@	behave tests
---
+++ tests/steps/delete.py
@@ -0,0 +1,26 @@
+from behave import given, when, then
+
+
+@when('the user deletes the person')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When the user deletes the person')
+
+@then('the person should not be in the triple store')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then the person should not be in the triple store')
+
+@when('the user deletes the group')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When the user deletes the group')
+
+@then('the group should not be in the triple store')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then the group should not be in the triple store')
+
+@when('the user deletes the organization')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When the user deletes the organization')
+
+@then('the organization should not be in the triple store')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then the organization should not be in the triple store')
